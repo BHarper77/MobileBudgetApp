2dfb073039e31ea9cfd0feb5ebf6eea548c06f3a
1700485 <55248329+1700485@users.noreply.github.com>
2020-01-28 11:35:50 +0000

    Updated wallet creation functionality

--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -22,6 +21,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        startActivity(new Intent(MainActivity.this, createWallet.class));
+        Intent intent = new Intent(MainActivity.this, createWallet.class);
+        startActivityForResult(intent, 1);
+    }
+
+    //Retrieving new created wallet from createWallet activity
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        TextView walletNameText = findViewById(R.id.walletName);
+        TextView walletBalanceText = findViewById(R.id.walletBalance);
+
+        if (requestCode == 1 && resultCode == RESULT_OK)
+        {
+            //Retriving bundle and extracting data
+            Bundle data = intent.getExtras();
+            String walletName = data.getString("walletName");
+            int walletBalance = data.getInt("walletBalance");
+
+            walletNameText.setText(walletName);
+            walletBalanceText.setText(String.valueOf(walletBalance));
+
+
+            //Toast.makeText(this, walletName, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/budgetapp/createWallet.java
+++ app/src/main/java/com/example/budgetapp/createWallet.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,15 +28,46 @@ public class createWallet extends AppCompatActivity implements  View.OnClickList
         EditText walletName = findViewById(R.id.walletName);
         EditText walletBalance = findViewById(R.id.walletBalance);
 
-        int balance = Integer.parseInt(walletBalance.getText().toString());
+        if (validate() == true)
+        {
+            int balance = Integer.parseInt(walletBalance.getText().toString());
 
-        //Creating new Wallet with inputted values
-        wallet = new Wallet(walletName.getText().toString(), balance);
+            //Creating new Wallet with inputted values
+            wallet = new Wallet(walletName.getText().toString(), balance);
 
-        //Sending class data back to main activity
-        Intent intent = new Intent(createWallet.this, MainActivity.class);
-        intent.putExtra("walletName", wallet.getWalletName());
-        intent.putExtra("walletBalance", wallet.getBalance());
-        startActivity(intent);
+            //Sending class data back to main activity
+            Intent intent = new Intent();
+            intent.putExtra("walletName", wallet.getWalletName());
+            intent.putExtra("walletBalance", wallet.getBalance());
+            this.setResult(RESULT_OK, intent);
+
+            finish();
+        }
+    }
+
+    //Validating wallet creation fields
+    public boolean validate() {
+        EditText walletName = findViewById(R.id.walletName);
+        String walletNameString = walletName.getText().toString();
+
+        EditText walletBalance = findViewById(R.id.walletBalance);
+        String walletBalanceString = walletBalance.getText().toString();
+
+        if (TextUtils.isEmpty(walletNameString))
+        {
+            walletName.setError("This field cannot be empty");
+
+            return false;
+        }
+        else if (TextUtils.isEmpty(walletBalanceString))
+        {
+            walletBalance.setError("This field cannot be empty");
+
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
